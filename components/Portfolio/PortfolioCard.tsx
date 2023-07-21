5ea10c8d22bf96f03aadf0ed1e931e231541ff73
Jesus Arciniega <dev.jesusarc@gmail.com>
2023-07-21 01:24:28 -0600

    Portfolio card component

--- components/Portfolio/PortfolioCard.tsx
+++ components/Portfolio/PortfolioCard.tsx
@@ -1,15 +1,51 @@
 import { Card, Col, Row, Button, Text } from "@nextui-org/react";
 
-const Eddi = () => {
+interface Props {
+  company: string;
+}
+
+const companiesData = {
+  Eddi: {
+    altText: "Eddi card",
+    description: "Frontend development with NextJS",
+    name: "Eddi",
+    thumbnail: "/eddi.png",
+    url: "https://www.edditheapp.com/",
+  },
+  FITB: {
+    altText: "Food in the box card",
+    description: "Shopify store managment / custom app development.",
+    name: "Food in the Box",
+    thumbnail: "/fitb.png",
+    url: "https://www.foodinthebox.com/",
+  },
+  Flex: {
+    altText: "Flex card",
+    description: "Migration from Shopify store to a headless ecommerce.",
+    name: "Flex",
+    thumbnail: "/flex.png",
+    url: "https://www.flexfits.com/",
+  },
+  WFR: {
+    altText: "WFR card",
+    description: "Shopify store frontend development.",
+    name: "We feed raw",
+    thumbnail: "/wfr.png",
+    url: "https://www.wefeedraw.com/",
+  },
+};
+
+const PortfolioCard = ({company}: Props) => {
+  const Company = companiesData[company as keyof typeof companiesData];
   return (
     <Card css={{ w: "100%", h: "400px" }}>
       <Card.Body css={{ p: 0 }}>
         <Card.Image
-          src="/eddi.png"
+          src={Company.thumbnail}
           objectFit="cover"
           width="100%"
           height="100%"
-          alt="Eddi card"
+          alt={Company.altText}
         />
       </Card.Body>
       <Card.Footer
@@ -27,10 +63,10 @@ const Eddi = () => {
             <Row>
               <Col>
                 <Text color="#d1d1d1" size={20}>
-                  Eddi
+                  {Company.name}
                 </Text>
                 <Text color="#d1d1d1" size={16} css={{ width: "120%" }}>
-                  Frontend development with NextJS
+                  {Company.description}
                 </Text>
               </Col>
             </Row>
@@ -42,7 +78,7 @@ const Eddi = () => {
                 auto
                 rounded
                 css={{ color: "#94f9f0", bg: "#94f9f026" }}
-                onClick={() => window.open("https://www.edditheapp.com/")}
+                onClick={() => window.open(Company.url)}
               >
                 <Text
                   css={{ color: "inherit" }}
@@ -61,4 +97,4 @@ const Eddi = () => {
   );
 };
 
-export default Eddi;
+export default PortfolioCard;
