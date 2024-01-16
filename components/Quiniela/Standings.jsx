10a295dacf60e05c8bb3a54a41a6ba03b79b27d2
Jesus Arciniega <dev.jesusarc@gmail.com>
2024-01-15 22:39:00 -0600

    Style update and logos

--- components/Quiniela/Standings.jsx
+++ components/Quiniela/Standings.jsx
@@ -1,40 +1,110 @@
+import { Table, Image } from '@nextui-org/react';
+
 const StangingsTable = (data) => {
-  console.log('From standings table: ', data.standings)
-  const standings = data?.standings ? Object.entries(data.standings.slice(0, 17)) : null;
+  const standings = data?.standings ? Object.entries(data.standings.slice(18, 35)) : null;
+  const columns = [
+    {
+      key: "rank",
+      label: "#",
+    },
+    {
+      key: "logo",
+      label: "",
+    },
+    {
+      key: "name",
+      label: "Equipo",
+    },
+    {
+      key: "matches",
+      label: "Jugados",
+    },
+    {
+      key: "won",
+      label: "Ganados",
+    },
+    {
+      key: "drawn",
+      label: "Empatados",
+    },
+    {
+      key: "lost",
+      label: "Perdidos",
+    },
+    {
+      key: "goals_scored",
+      label: "GF",
+    },
+    {
+      key: "goals_conceded",
+      label: "GC",
+    },
+    {
+      key: "goal_diff",
+      label: "DG",
+    },
+    {
+      key: "points",
+      label: "Puntos",
+    },
+  ];
 
   return (
-    <table>
-      <thead>
-        <th></th>
-        <th>Equipo</th>
-        <th>Jugados</th>
-        <th>Ganados</th>
-        <th>Empatados</th>
-        <th>Perdidos</th>
-        <th>Goles a Favor</th>
-        <th>Goles en Contra</th>
-        <th>Diferencia de Goles</th>
-        <th>Puntos</th>
-      </thead>
-      <tbody>
-        {
-          standings?.map(([key, { rank, name, matches, won, drawn, lost, goals_scored, goals_conceded, goal_diff, points }]) => (
-            <tr key={key}>
-              <td>{rank}</td>
-              <td>{name}</td>
-              <td>{matches}</td>
-              <td>{won}</td>
-              <td>{drawn}</td>
-              <td>{lost}</td>
-              <td>{goals_scored}</td>
-              <td>{goals_conceded}</td>
-              <td>{goal_diff}</td>
-              <td>{points}</td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
+    <>
+      <Table
+        aria-label="Liga MX Standings"
+        css={{
+          height: "auto",
+          minWidth: "100%",
+          '.clasified': {
+            color: '#2eff2e'
+          },
+          '.playin': {
+            color: '#dbdba0'
+          }
+        }}
+        selectionMode="none"
+      >
+        <Table.Header columns={columns}>
+          {(column) => (
+            <Table.Column
+              key={column.key}
+            >
+              {column.label}
+            </Table.Column>
+          )}
+        </Table.Header>
+        <Table.Body items={standings}>
+          {
+            standings?.map(([key, { rank, team_id, name, matches, won, drawn, lost, goals_scored, goals_conceded, goal_diff, points }]) => {
+              const logoURL = `/teams/${team_id}.png`
+              return (
+                <Table.Row key={key}>
+                  <Table.Cell><span className={parseInt(rank) < 5 ? "clasified" : parseInt(rank) < 13 ? "playin" : ""}>{rank}</span></Table.Cell>
+                  <Table.Cell>
+                    <Image
+                      src={logoURL}
+                      alt="Default Image"
+                      width={30}
+                      height={30}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>{name}</Table.Cell>
+                  <Table.Cell>{matches}</Table.Cell>
+                  <Table.Cell>{won}</Table.Cell>
+                  <Table.Cell>{drawn}</Table.Cell>
+                  <Table.Cell>{lost}</Table.Cell>
+                  <Table.Cell>{goals_scored}</Table.Cell>
+                  <Table.Cell>{goals_conceded}</Table.Cell>
+                  <Table.Cell>{goal_diff}</Table.Cell>
+                  <Table.Cell><b>{points}</b></Table.Cell>
+                </Table.Row>
+              )
+            })
+          }
+        </Table.Body>
+      </Table>
+    </>
   )
 }
 
--- pages/quiniela/index.jsx
+++ pages/quiniela/index.jsx
@@ -1,5 +1,6 @@
 // pages/index.js
 import { useEffect, useState } from "react";
+import { Grid, Card, Col, Text } from "@nextui-org/react";
 import MatchesTable from "../../components/Quiniela/MatchesTable";
 import StangingsTable from "../../components/Quiniela/Standings";
 
@@ -40,13 +41,43 @@ function Quiniela() {
   
 
   return (
-    <div>
-      <h1>Jornada {round} - Liga MX</h1>
-      {matches && <MatchesTable matches={matches} />}
-      <hr />
-      <h2>Tabla general</h2>
-      {standings && <StangingsTable standings={standings} />}
-    </div>
+    <Grid.Container gap={2}>
+      <Grid md={4} xs={12}>
+        <Card>
+          <Card.Header>
+            <Col>
+              <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+                Liga MX
+              </Text>
+              <Text h4 color="white">
+                Jornada {round}
+              </Text>
+            </Col>
+          </Card.Header>
+          <Card.Divider />
+          <Card.Body>
+            {matches && <MatchesTable matches={matches} />}
+          </Card.Body>
+        </Card>
+      </Grid>
+      <Grid md={8} xs={12}>
+        <Card>
+          <Card.Header>
+            <Col>
+              <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+                Tabla General
+              </Text>
+            </Col>
+          </Card.Header>
+          <Card.Divider />
+          <Card.Body>
+            {standings && <StangingsTable standings={standings} />}
+          </Card.Body>
+        
+        </Card>
+      </Grid>
+      
+    </Grid.Container>
   );
 }
 
